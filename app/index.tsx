4aabc69a2a832230cd23d39b5a103a63c68f7fd9
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-07-06 06:36:24 +0700

    flex

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
+import FlexboxMenu from "@/components.tsx/FlexboxMenu";
 import { Link, router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -11,12 +12,35 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button title="Go to Next Page" onPress={() => router.push("/page2")} />
-      <Link
-        href="/page2"
-      >
-        <Text >Go to Next Page</Text>
+
+      <Text>Flexbox Menu</Text>  
+      <FlexboxMenu />
+
+      <Link href="/flexbox/ex01" style={styles.link}>
+        <Text style={styles.linkText}>1</Text>
       </Link>
+
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginTop: 24,
+  },
+  link: {
+    margin: 16,
+    padding: 16,
+    backgroundColor: "#2196F3",
+    borderRadius: 5,
+  },
+  linkText: {
+    color: "#fff",
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
