da0c572bcc11489c102973d756423da639d85797
Chavalit Koweerawong <chavalit.kow@gmail.com>
2025-07-06 07:01:54 +0700

    Components

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,5 @@
-import FlexboxMenu from "@/components.tsx/FlexboxMenu";
-import { Link, router } from "expo-router";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -13,11 +12,11 @@ export default function Index() {
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
 
-      <Text>Flexbox Menu</Text>  
-      <FlexboxMenu />
+      {/* <Text>Flexbox Menu</Text>  
+      <FlexboxMenu /> */}
 
-      <Link href="/flexbox/ex01" style={styles.link}>
-        <Text style={styles.linkText}>1</Text>
+      <Link href="/test-components" style={styles.link}>
+        <Text style={styles.linkText}>Test Components</Text>
       </Link>
 
 
@@ -26,21 +25,15 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    marginTop: 24,
-  },
   link: {
-    margin: 16,
-    padding: 16,
+    margin: 10,
+    padding: 10,
     backgroundColor: "#2196F3",
     borderRadius: 5,
   },
   linkText: {
     color: "#fff",
-    fontSize: 18,
+    fontSize: 16,
     textAlign: "center",
   },
 });
